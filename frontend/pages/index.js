61577e9d822a8946a63cb483c9b91357ec856794
Patrick Olvera <patrickolvera@gmail.com>
2019-01-19 11:40:42 -0600

    Feat: Basic scaffolding for core components

---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+const Home = props => (
+  <div>
+    <h2>Hello Sick Fits!</h2>
+  </div>
+);
+
+export default Home;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+const Sell = props => (
+  <div>
+    <h2>Hello Sell Page!</h2>
+  </div>
+);
+
+export default Sell;
